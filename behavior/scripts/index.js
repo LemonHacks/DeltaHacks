a117c94880e6ce8a5f5e28581a66297986575b29
annalorimer <aemhlori@uwaterloo.ca>
2017-01-29 02:34:05 -0500

    a brand new index.js at 2 in the morning woo

--- behavior/scripts/index.js
+++ behavior/scripts/index.js
@@ -1,66 +1,31 @@
 'use strict'
 
-//const PasswordHowto = require('./lib/password_howto')
-
 exports.handle = function handle(client) {
-  // Create steps
-  const passwordHowto = client.createStep({
-      satisfied() {
-          console.log("satisfied is running...")
-          let charLengthExists = Boolean(client.getConversationState().charLength)
-          console.log("char length", charLengthExists)
-          return charLengthExists
-      },
+  const checkStrength = client.createStep({
+    satisfied() {
+      return Boolean(client.getConversationState().passLength)
+    },
 
-      extractInfo() {
-          console.log("extractInfo is running...")
-          console.log("client message is ", client.getMessagePart())
-          console.log("Conversation state: ", client.getConversationState())
-          let num = client.getFirstEntityWithRole(client.getMessagePart(), 'number/passlength')
-          console.log("num: ", num)
-          if (num) {
-              client.updateConversationState('charLength', num)
-              console.log('NUMBER is defined')
-          }
-          console.log("Conversation state: ", client.getConversationState())
-      },
+    extractInfo() {
+     const length = client.getFirstEntityWithRole(client.getMessagePart(), 'number/passlength')
+      if (length) {
+        client.updateConversationState({
+          passLength: length,
+        })
+        console.log('User wants to check passLength:', length.value)
+      }
+    },
 
-      prompt() {
-        console.log("prompt is running...")
-        if (parsetInt(client.getConversationState().charLength) > 8) {
-            client.addResponse('password/good')
-        } else client.addResponse('password/bad')
-        client.done()
+    prompt() {
+      client.addResponse('password/good')
+      client.done()
     },
   })
 
-  // const passwordTips = client.createStep({
-  //     satisfied() {
-  //         return false
-  //     },
-  //
-  //     prompt() {
-  //       //client.addResponse('password/tips')
-  //       client.done()
-  //     },
-  // })
-  // prompt(callback){
-  //     const environment = client.getCurrentApplicationEnvironment()
-  //     PasswordHowto()
-  //     console.log('Voila')
-  // }
-
   client.runFlow({
-    classifications: {
-      // map inbound message classifications to names of streams
-    },
-    autoResponses: {
-      // configure responses to be automatically sent as predicted by the machine learning model
-    },
+    classifications: {},
     streams: {
-      main: 'passwordhowto',
-      passwordhowto: [passwordHowto],
-      //end: [untrained],
+      main: 'checkStrength',
     }
   })
 }
