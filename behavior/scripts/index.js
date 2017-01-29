1efbf0cdfe7f8fce0b7dc9f6917e7a0d4f16b576
annalorimer <aemhlori@uwaterloo.ca>
2017-01-29 02:41:26 -0500

    gah

--- behavior/scripts/index.js
+++ behavior/scripts/index.js
@@ -3,20 +3,21 @@
 exports.handle = function handle(client) {
   const checkStrength = client.createStep({
     satisfied() {
+        console.log("satisfied...")
       return Boolean(client.getConversationState().passLength)
     },
 
     extractInfo() {
      const length = client.getFirstEntityWithRole(client.getMessagePart(), 'number/passlength')
+     console.log("extract...")
       if (length) {
-        client.updateConversationState({
-          passLength: length,
-        })
+        client.updateConversationState('passLength', length)
         console.log('User wants to check passLength:', length.value)
       }
     },
 
     prompt() {
+        console.log("prompt..")
       client.addResponse('password/good')
       client.done()
     },
